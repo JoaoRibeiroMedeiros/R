c6697b40201fe8d9c9ebcb4b10892295914ff642
João Ribeiro Medeiros <joao_rmedeiros@yahoo.com.br>
2021-04-05 17:01:09 -0300

    New Optimized version for the 3D histogram
    A lot faster then original algorithm, now searches the indexes by halfing the sample space, in current form only works for numer of breaks = 2^N

---
+++ Hist3D.R
@@ -0,0 +1,83 @@
+Hist3D<- function(x,y, breaks){ #breaks is 2^N
+    
+    len<- length(x) 
+    
+    rangeX <- max(x) - min(x)
+    
+    rangeY <- max(y) - min(y)
+    
+    N<-log(breaks)/log(2)
+    
+    intervalox <- rangeX/breaks
+    
+    intervaloy <- rangeY/breaks
+    
+    midsxx<- matrix(ncol = breaks, nrow = breaks)
+    
+    midsyy<- matrix(ncol = breaks, nrow = breaks)
+        
+    breaksx<- seq( min(x) ,  max(x) , intervalox ) # length BREAKS +1
+    
+    breaksy<- seq( min(y) ,  max(y) , intervaloy ) # length BREAKS +1
+    
+    pesos<- matrix(ncol = breaks, nrow = breaks)
+    pesos<- is.na(pesos) - 0L
+    pesos<- pesos - 1
+
+    
+    for( i in 1: len)
+    {
+        indicex<- 1
+        indicey<- 1
+        
+        b<- breaks
+        
+        for( j in 1:N )
+        {
+        	b<- b/2
+            if(breaksx[indicex+b-1] <= x[i] )
+            {
+                binario <-1
+            }
+            if( x[i] < breaksx[indicex+b-1 ] )
+            {
+                binario <- 0 
+            }
+          
+          indicex<- indicex + b*binario
+        }
+        
+         b<- breaks
+         
+         for( j in 1:N )
+        {
+        	b<- b/2
+            if(breaksy[indicey+b-1] <= y[i] )
+            {
+                binario <-1
+            }
+            if( y[i] < breaksy[indicey+b-1 ] )
+            {
+                binario <- 0 
+            }
+          
+          indicey<- indicey + b *binario
+            
+        }
+        
+        pesos[indicex,indicey] <- pesos[indicex,indicey] + 1
+        
+    }
+    
+    pesos <- pesos /len
+    
+    pesos <- pesos / (( breaksx[2] - breaksx[1] ) * ( breaksy[2] - breaksy[1] ))
+    
+    #return(pesos)
+    
+    final<-list("density"=pesos,"breaksx"=breaksx,"breaksy"=breaksy )
+    
+    return(final)
+    
+}
+
